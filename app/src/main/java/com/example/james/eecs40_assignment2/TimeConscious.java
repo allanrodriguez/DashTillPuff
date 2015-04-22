b0354e3070283a74d32464fd6504c90980d12f20
James <jortizlu@uci.edu>
2015-04-22 15:41:24 -0700

    added screen orientation lock in <activity> of AndroidManifest.xml

--- app/src/main/java/com/example/james/eecs40_assignment2/TimeConscious.java
+++ app/src/main/java/com/example/james/eecs40_assignment2/TimeConscious.java
@@ -1,8 +1,11 @@
 package com.example.james.eecs40_assignment2;
 
+import android . graphics . Canvas ;
 /**
  * Created by James on 4/22/2015.
  */
+
 public interface TimeConscious
 {
+	public void tick ( Canvas canvas ) ;
 }
---
+++ app/src/main/java/com/example/james/eecs40_assignment2/Trajectory.java
@@ -0,0 +1,43 @@
+package com.example.james.eecs40_assignment2;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+
+
+/**
+ * Created by James on 4/22/2015.
+ */
+public class Trajectory implements TimeConscious
+{
+	private ArrayList<Point2D> points;
+
+
+	@Override
+	public void tick(Canvas canvas)
+	{
+		// As time ticks , append more points to the trajectory and
+		// discard those points that have crossed the entire
+		// span of the screen .
+
+		draw(canvas);
+	}
+
+	private void draw(Canvas c)
+	{
+
+
+		Path path = new Path();
+		path.moveTo(); // Move to first point
+		for (int i = 1; i < points.size(); ++i)
+		{
+
+			path.lineTo(p.x, p.y);
+		}
+		Paint paint = new Paint();
+
+		// Set paint color , alpha , line width , dashed style , etc .
+
+		c.drawPath(path, paint);
+	}
+}
